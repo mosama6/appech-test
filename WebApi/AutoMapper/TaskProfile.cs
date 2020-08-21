94f522efaff7984893a49504bfca681ffea7ccd7
Osama <hp@DESKTOP-PG2HLBQ>
2020-08-21 20:41:07 +0500

    Db Changes

---
+++ WebApi/AutoMapper/TaskProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Domain.Commands;
+using Domain.DataModels;
+using Domain.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task = Domain.DataModels.Task;
+
+namespace WebApi.AutoMapper
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<CreateTaskCommand, Task>();
+            CreateMap<UpdateTaskCommand, Task>();
+            CreateMap<Task, TaskVm>();
+        }
+    }
+}
